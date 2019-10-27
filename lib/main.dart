dd9f97af827681cda36246d11accd8c24574e640
Zeeshan Noor <bshani15@gmail.com>
2019-10-27 14:02:47 +0500

    #2.2 Flutter Container widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,11 @@
-import "package:flutter/material.dart";
-import './app_screens/first_screen.dart';
+import 'package:flutter/material.dart';
+import './app_screens/home.dart';
 
-void main() => runApp(new MyFlutterApp());
+void main()
+{
+  runApp(MaterialApp(
+    title: "Exploring UI Widgets",
+    home: Home(),
 
-
-class MyFlutterApp extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      debugShowCheckedModeBanner: false,
-        title: "My Flutter App",
-        home: Scaffold(
-          appBar: AppBar(title: Text("My First App Screen"),),
-          body: FirstScreen()
-        )
-    );
-  }
-
-
+  ));
 }
